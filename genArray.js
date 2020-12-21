76327939117e6a0ef9521a8051acce48d0fedcf6
tingyu3 <tingyu3@staff.sina.com.cn>
2020-12-21 10:28:34 +0800

    add

---
+++ genArray.js
@@ -0,0 +1,4 @@
+let ar = Array.from({ length: 3},() => Array(3))
+// Array.form中可以放入伪数组对象或者可迭代对象
+// 第二个函数，新数组中的每个元素都会执行该回调
+
--- insertAdjacentHTML.js
+++ insertAdjacentHTML.js
@@ -1,30 +1,30 @@
-        if (typeof HTMLElement !== "undefined" && !HTMLElement.prototype.insertAdjacentElement) {
-            HTMLElement.prototype.insertAdjacentElement = function(where, parsedNode) {
-                switch (where.toLowerCase()) {
-                    case 'beforebegin':
-                        this.parentNode.insertBefore(parsedNode, this)
-                        break;
-                    case 'afterbegin':
-                        this.insertBefore(parsedNode, this.firstChild);
-                        break;
-                    case 'beforeend':
-                        this.appendChild(parsedNode);
-                        break;
-                    case 'afterend':
-                        if (this.nextSibling)
-                            this.parentNode.insertBefore(parsedNode, this.nextSibling);
-                        else this.parentNode.appendChild(parsedNode);
-                        break;
-                }
-            }
-            HTMLElement.prototype.insertAdjacentHTML = function(where, htmlStr) {
-                var r = this.ownerDocument.createRange();
-                r.setStartBefore(this);
-                var parsedHTML = r.createContextualFragment(htmlStr);
-                this.insertAdjacentElement(where, parsedHTML)
-            }
-            HTMLElement.prototype.insertAdjacentText = function(where, txtStr) {
-                var parsedText = document.createTextNode(txtStr)
-                this.insertAdjacentElement(where, parsedText)
-            }
+if (typeof HTMLElement !== "undefined" && !HTMLElement.prototype.insertAdjacentElement) {
+    HTMLElement.prototype.insertAdjacentElement = function(where, parsedNode) {
+        switch (where.toLowerCase()) {
+            case 'beforebegin':
+                this.parentNode.insertBefore(parsedNode, this)
+                break;
+            case 'afterbegin':
+                this.insertBefore(parsedNode, this.firstChild);
+                break;
+            case 'beforeend':
+                this.appendChild(parsedNode);
+                break;
+            case 'afterend':
+                if (this.nextSibling)
+                    this.parentNode.insertBefore(parsedNode, this.nextSibling);
+                else this.parentNode.appendChild(parsedNode);
+                break;
         }
+    }
+    HTMLElement.prototype.insertAdjacentHTML = function(where, htmlStr) {
+        var r = this.ownerDocument.createRange();
+        r.setStartBefore(this);
+        var parsedHTML = r.createContextualFragment(htmlStr);
+        this.insertAdjacentElement(where, parsedHTML)
+    }
+    HTMLElement.prototype.insertAdjacentText = function(where, txtStr) {
+        var parsedText = document.createTextNode(txtStr)
+        this.insertAdjacentElement(where, parsedText)
+    }
+}
